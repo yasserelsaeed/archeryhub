fa7b42853380ffb440b323b79396dfe6452f1ff2
Yasser <you@example.com>
2020-02-02 14:00:49 -0500

    another attempt

--- js/carousel.js
+++ js/carousel.js
@@ -12,7 +12,7 @@ const nextbtn = document.querySelector('#nextbtn');
 
 
 let counter = 1;
-let size =  document.querySelector('#last-clone').clientWidth;
+let size =  document.querySelector('.carousel-slide img').clientWidth;
 
 carouselSlide.style.transform = 'translateX(' + (- size * counter) + 'px)';
 
